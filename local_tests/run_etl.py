3c05d5e99bc5e9ce39f42aba02cc3c10f7f1fa66
Viktor Makarov <victor.makarov@me.com>
2024-02-01 10:51:49 +0300

    fix connection name

--- local_tests/run_etl.py
+++ local_tests/run_etl.py
@@ -8,7 +8,7 @@ from dags.google_play_review_lib.load import GooglePlayReviews
 if __name__ == "__main__":
     """Для теста"""
 
-    clickhouse_http = Connection.get_connection_from_secrets("clickhouse_http")
+    clickhouse_http = Connection.get_connection_from_secrets("clickhouse_http_local")
     GooglePlayReviews(
         clickhouse_http=clickhouse_http,
         end_date_excl=datetime.now(pytz.utc).date(),
